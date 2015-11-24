24d7c51ca6b00ed10c02c017c0456b2018dfa031
alu0100782851 <alu0100782851@ull.edu.es>
2015-11-24 19:19:10 +0000

    Añadiendo funciones para insertar en la lista

--- lib/prct_09/Listas.rb
+++ lib/prct_09/Listas.rb
@@ -13,4 +13,33 @@ class Lista
         @contador=0     #variable que determina el tamaño actual de la lista
     end
     
+    def insert_head(nodo)
+        
+        if empty == true            #Si la lista esta vacia insertamos el elemento en la lista vacia
+            nodo.previo = @tail
+            nodo.siguiente = @head
+            @head = nodo
+            @tail = nodo
+        else                        #Si no esta vacia insertamos por el head 
+            nodo.previo = @head
+            nodo.siguiente = nil
+            @head.siguiente = nodo
+            @head = nodo
+        end
+        @contador = @contador + 1   #Aumentamos variable que indica tamaño de la lista 
+        
+    end
+    
+    def insert_tail(nodo)
+        if empty                    #Si esta vacia insertamos en lista vacia
+            insert_head(nodo)
+        else                        #Si no insertamos por el tail
+            nodo.siguiente = @tail
+            nodo.previo = nil
+            @tail.previo = nodo
+            @tail = nodo
+            @contador = @contador + 1
+        end
+    end
+    
 end
