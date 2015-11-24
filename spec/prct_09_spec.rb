a383ea9eae05dec7464fc6c8975d12550e23ec3e
alu0100782851 <alu0100782851@ull.edu.es>
2015-11-24 18:51:52 +0000

    Añadiendo pruebas para los nodos

--- spec/prct_09_spec.rb
+++ spec/prct_09_spec.rb
@@ -14,4 +14,35 @@ describe Bibliografias do
     @libro_4 = Bibliografias.new("Scott Chacon, Dave Astels, Bryan Helmkamp, Dan North, Zach Dennis, Aslak Hellesoy","The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends (The Facets of Ruby)","December 25, 2010","4")
   end
 
+    #Pruebas con la lista y con los nodos
+  describe Lista do
+  
+    before :each do
+      #Nodos que contienen las referencias para hacer las pruebas
+      @nodo1 = Nodo.new(@libro_1)
+      @nodo2 = Nodo.new(@libro_2)
+      @nodo3 = Nodo.new(@libro_3)
+      @nodo4 = Nodo.new(@libro_4)
+
+      @lista = Lista.new
+      
+      @lista_1 = Lista.new
+      @lista_1.insert_head(@nodo1)
+      @lista_1.insert_head(@nodo2)
+      @lista_1.insert_head(@nodo3)
+      @lista_1.insert_head(@nodo4)
+      
+    end
+
+    describe "Nodo" do
+   
+      it "#Debe existir un Nodo de la lista con sus datos y su siguiente" do
+        expect(@nodo1.class).to eq(Nodo)
+      end 
+  
+    end
+    
+    
+  end
+    
 end
