4d17c7396c9ffb8de756fd7ffd02957c292ed9e2
alu0100782851 <alu0100782851@ull.edu.es>
2015-11-24 19:13:32 +0000

    Añadiendo modulo comparable a la clase, asi como las funciones necesarias para poder realizar las comparaciones

--- lib/prct_09/Bibliografias.rb
+++ lib/prct_09/Bibliografias.rb
@@ -2,7 +2,7 @@ class Bibliografias
 
     attr_accessor :autores , :titulo , :publicacion, :edicion #geters y serters 
 
-    #include Comparable
+    include Comparable
     
     def initialize(autores, titulo, publicacion, edicion)
         @autores = autores
@@ -10,5 +10,23 @@ class Bibliografias
         @publicacion = publicacion
         @edicion = edicion
     end
+    
+    def <=>(other)
+	
+		return nil unless other.is_a?Bibliografias  #Devuelve nil si se cumple que other no es un objeto de tipo bibliografia
+		
+		@autores  <=> other.autores
+
+    end
+	
+	def ==(other)
+		
+		if other.is_a?Bibliografias
+            @titulo == other.titulo && @autores == other.autores && @publicacion == other.publicacion &&  @edicion == other.edicion
+		else
+		    false
+		end
+
+	end
     
 end
